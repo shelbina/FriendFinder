43a17b922f4ffcb964917d7df701a18b5accc8b0
shelbina <deel.shelby@gmail.comgit clone git@github.com:shelbina/mayeleven.gitgit@github.com:shelbina/mayeleven.git$ git config --global user.email deel.shelby@gmail.com>
2017-08-14 23:11:56 -0400

    update routes and server

--- app/routing/apiroutes.js
+++ app/routing/apiroutes.js
@@ -1,23 +1,45 @@
-module.exports = function(app){ 
-    //A GET route with the url /api/friends. used to display a JSON of all possible friends.
-    app.get("/api/friends", function(req, res){
-        res.json(reservations);
-    });
-    
-    //// A POST routes /api/friends. used to handle incoming survey results.
-    // This route will also be used to handle the compatibility logic.
-    app.get("/api/waitlist", function(req, res){
-        res.json(waitList);
-    });
-    
-    //post for making reservation
-    
-    app.post("/api/reserve", function(req, res){
-        console.log(req);
-        var reserve = req.body;
-        reservations.push(reserve);
-        
-        
-    });
+var friendData = require("../data/friends.js");
 
+module.exports = function(app){ 
+    app.get("/api/friends", function(req, res){
+        res.json(friendData);
+    })
+
+    app.post("/api/friends", function(req, res){
+        console.log(req);
+        var newFriend = req.body;
+
+        for(var i = 0; i < newFriend.scores.length; i++){
+            if(newFriend.scores[i] == "1 Nah man!"){
+                newFriend.scores[i] = 1;
+            } else if(newFriend.scores[i] == "5 Heck yeah!"){
+                newFriend.scores[i] = 5;
+            } else {
+                newFriend.scores[i] = parseInt(newFriend.scores[i]);
+            }
+        }
+        var differenceArray = [];
+
+        for (var i = 0; i < friendData.length; i++) {
+            var comparedFriend = friendData[i];
+            var totalDifference = 0;
+
+            for (var k = 0; k < comparedFriend.scores.length; k++){
+                var differenceScore = Math.abs(comparedFriend.scores[k] - newFriend.scores[k]);
+                totalDifference += differenceScore;
+            }
+            differencesArray[i] = totalDifference;
+        }
+        var bestFriendNum = differencesArray[0];
+        var bestFriendIndex = 0;
+
+        for(var i = 1; i < differencesArray.length; i++){
+            if(differencesArray[i] < bestFriendNum){
+                bestFriendNum = differenceArray[i];
+                bestFriendIndex = i;
+            }
+        }
+        friendData.push(newFriend);
+        res.json(friendData[bestFriendIndex]);
+    })
 }
